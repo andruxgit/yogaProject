e179a49df8f5b4124db820b5042064d3c8a98e70
andruxgit <jobandrux@yandex.ru>
2018-11-29 04:15:12 +0300

    2901h

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', function () {
   'use strict';
+  
   let tab = document.querySelectorAll('.info-header-tab'),
     info = document.querySelector('.info-header'),
     tabContent = document.querySelectorAll('.info-tabcontent');
@@ -21,18 +22,61 @@ window.addEventListener('DOMContentLoaded', function () {
     }
   }
   //при событии клик на табе происходит отображение нужного контента
- info.addEventListener('click', function(event){
+  info.addEventListener('click', function (event) {
     let target = event.target;
-    if (target && target.classList.contains('info-header-tab')){
-       for (let i= 0;i<tabContent.length; i++){
-         if (target == tab[i]){
-           hideTabContent(0);
-           showTabContent(i);
-           break;
-         }
-       }
+    if (target && target.classList.contains('info-header-tab')) {
+      for (let i = 0; i < tabContent.length; i++) {
+        if (target == tab[i]) {
+          hideTabContent(0);
+          showTabContent(i);
+          break;
+        }
+      }
     }
 
- });
+  });
+  //таймер обратного отсчета 
+  let deadLine = '2018/11/30';
 
+  // console.log(Date.parse(new Date()))
+//Получение обьекта с небходимыми параметрами времени определяющими временной отрезок до события
+  function getTimeRemaning(endTime) {
+    let t = Date.parse(endTime) - Date.parse(new Date()),
+      seconds = Math.round((t / 1000) % 60),
+      minutes = Math.round((t / 1000 / 60) % 60),
+      hours = Math.round(t / (1000 * 60 * 60));
+    return {
+      'total': t,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+// тело таймера (отображение на странице и изменение каждую секунду сколко времени до события)
+//id - контейнер таймера и endTime - время события
+  function setClock(id, endTime) {
+    let timer = document.getElementById(id),
+      hours = timer.querySelector('.hours'),
+      minutes = timer.querySelector('.minutes'),
+      seconds = timer.querySelector('.seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      let t = getTimeRemaning(endTime);
+    if(t.total > 0){ 
+      hours.textContent = (t.hours < 10) ? '0' + t.hours : t.hours;
+      minutes.textContent = (t.minutes < 10) ? '0' + t.minutes : t.minutes;
+      seconds.textContent = (t.seconds < 10) ? '0' + t.seconds : t.seconds;
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    } else {
+      hours.textContent = '00';
+      minutes.textContent = '00';
+      seconds.textContent = '00';
+    }
+    }
+  }
+
+  setClock('timer', deadLine);
 });
